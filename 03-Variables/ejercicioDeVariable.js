97967e90b779f2948dbcf356bae50907fa60e2ff
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-07-06 18:12:22 -0500

    ✨ [Introduce] variables

---
+++ 03-Variables/ejercicioDeVariable.js
@@ -0,0 +1,54 @@
+// Declara una variable
+var pizza;
+console.log("line 2 - pizza", pizza);
+
+// Declarar y asignar una variable
+var taco = "suadero";
+console.log("line 6 - taco", taco);
+
+// Asignar el valor de primer variable
+pizza = "peperoni";
+console.log("line 10 - pizza", pizza)
+
+// Declara una variable por cada tipo de dato y asignale el valor correspondiente, solo los tipos primitivos
+var string = 'Soy un string / cadena de caracteres';
+var number = 4;
+var nulo = null;
+var indefinido = undefined;
+var verdadero = true;
+// var falso = false;
+var symbol = Symbol("Simbolo");
+
+// Imprime en consola una suma de dos números
+console.log(1+1);
+
+// Declara y asigna dos variables númericas e imprime en consola la suma de esas variables
+
+// variable 1
+var primerNumero = 5;
+// variable 2
+var segundoNumero = 10;
+// suma en consola
+console.log(primerNumero + segundoNumero) // 15
+
+// Suma
+console.log(4+4);
+// Resta
+console.log(4-4);
+// Multiplicación
+console.log(4*4);
+// División
+console.log(4/4);
+// Remanente
+console.log(21%5);
+// Exponentes
+console.log(4**2);
+
+
+var tuNombre = prompt('¿Cuál es tu nombre?');
+
+document.write("Hola" + " " + tuNombre);
+
+
+var edad = prompt('¿Cuál es tu edad?');
+console.log(edad);
---
+++ 03-Variables/variables.js
@@ -0,0 +1,28 @@
+// Estoy declarando la variable, pero no le estoy asignando ningun valor;
+var nombreDeLaVariable;
+console.log("nombreDeLaVariable", nombreDeLaVariable); // undefined
+
+nombreDeLaVariable = 'Esta es mi primer variable';
+console.log("nombreDeLaVariable", nombreDeLaVariable); // 'Esta es mi primer variable'
+
+
+// Estoy declarando y asignando una variable.
+var primerNombre = 'Gerardo';
+var primernombre = 'Gerardo';
+
+
+// camelCase
+
+// snake_case
+var otro_nombre_de_la_variable;
+
+// kebab-case
+// var nombre-de-la-variable; esto no es válido en JS
+
+// puedes escriber todo en mayusculas
+var ESTA_ES_CONSTANTE = 5;
+
+typeof ESTA_ES_CONSTANTE
+typeof(ESTA_ES_CONSTANTE)
+
+console.log(typeof ESTA_ES_CONSTANTE) // number
