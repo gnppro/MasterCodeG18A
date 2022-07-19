3c33b62dd1ab4eb332957a632807efe36d4656b0
Gerardo Nava Pereda <gerardo.nava.pereda@gmail.com>
2022-07-18 21:34:56 -0500

    funcional

---
+++ 13-RepasoS2/funcional.js
@@ -0,0 +1,13 @@
+function sumaDeDosNumeros(numero1, numero2) {
+  return numero1 + numero2
+}
+
+function restaDeDosNumeros(numero1, numero2) {
+  return numero1 - numero2
+}
+
+function multiplicacionDeDosNumeros(n1, n2) {
+  return n1 * n2
+}
+
+console.log(multiplicacionDeDosNumeros(sumaDeDosNumeros(2, 2), restaDeDosNumeros(4, 2)))
